5a79539049492c663e88c9470f89c883d22499cb
Sepehr Safari <safari.sepehr@gmail.com>
2023-08-04 03:58:58 +0330

    edit navbar and add user details

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -89,7 +89,7 @@ export default function Navbar() {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right divide-y divide-gray-100 rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-64 origin-top-right divide-y divide-gray-100 rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <Menu.Item as="a">
                         <img
                           className="mt-4 mx-auto h-24 w-24 flex-shrink-0 rounded-full"
