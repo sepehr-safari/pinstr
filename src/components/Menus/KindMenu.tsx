f028715bd9c98f50e5dd4c984f5dab666acbd5e3
Sepehr Safari <safari.sepehr@gmail.com>
2023-08-05 15:58:08 +0330

    edit kinds description

--- src/components/Menus/KindMenu.tsx
+++ src/components/Menus/KindMenu.tsx
@@ -5,15 +5,16 @@ import { MenuTemplate } from '@/components/Menus';
 const kinds = [
   {
     name: 'Generic Boards (Kind 33888)',
-    description: 'Pins of everything (e.g. links, images, videos, texts, etc.)',
+    description:
+      'Boards of everything (e.g. links, images, videos, texts, etc.)',
   },
   {
     name: 'Nostr Profiles (Kind 30000)',
-    description: 'Pins of nostr profiles (npub1...)',
+    description: 'Boards of nostr profiles (npub1...)',
   },
   {
     name: 'Nostr Notes (Kind 30001)',
-    description: 'Pins of nostr notes (note1...)',
+    description: 'Boards of nostr notes (note1..., naddr1...)',
   },
 ];
 
