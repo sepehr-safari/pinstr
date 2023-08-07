f109e00184188ed44535f9cad22f61d695d2e4e8
Sepehr Safari <safari.sepehr@gmail.com>
2023-08-07 15:28:57 +0330

    edit overlay darkness

--- src/components/CreateSlideover.tsx
+++ src/components/CreateSlideover.tsx
@@ -24,7 +24,7 @@ export default function CreateSlideover({ open, setOpen }: Props) {
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-black bg-opacity-50 transition-opacity" />
+          <div className="fixed inset-0 bg-black bg-opacity-70 transition-opacity" />
         </Transition.Child>
 
         <div className="fixed inset-0" />
