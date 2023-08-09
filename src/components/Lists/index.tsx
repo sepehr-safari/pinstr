42db2952645f1d3b785ccc70d5fed8e38875710f
Sepehr Safari <safari.sepehr@gmail.com>
2023-08-09 18:19:17 +0330

    edit exports for list components

--- src/components/Lists/index.tsx
+++ src/components/Lists/index.tsx
@@ -1,2 +1,5 @@
-export { default as InCard } from './InCard';
+export { default as LinkGrid } from './LinkGrid';
+export { default as NoteGrid } from './NoteGrid';
 export { default as PeopleGrid } from './PeopleGrid';
+export { default as PictureGrid } from './PictureGrid';
+export { default as VideoGrid } from './VideoGrid';
