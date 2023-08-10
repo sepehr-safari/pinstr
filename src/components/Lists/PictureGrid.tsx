51cd2a233ce600123efb241f5483d73ccc5fae8b
Sepehr Safari <safari.sepehr@gmail.com>
2023-08-10 15:45:31 +0330

    edit picture and video grid effects

--- src/components/Lists/PictureGrid.tsx
+++ src/components/Lists/PictureGrid.tsx
@@ -39,24 +39,33 @@ export default function PictureGrid() {
       className="mt-16 grid grid-cols-1 gap-x-6 gap-y-14 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-2 xl:gap-x-8 2xl:grid-cols-3"
     >
       {files.map((file) => (
-        <li key={file.source} className="relative">
-          <div className="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100">
-            <img
-              src={file.source}
-              alt=""
-              className="pointer-events-none object-cover group-hover:opacity-75"
-            />
+        <li
+          key={file.source}
+          className="group relative rounded-md hover:bg-gray-50 transition-all ease-in-out duration-500 hover:shadow-md"
+        >
+          <div className="transition-transform ease-in-out duration-700 group-hover:scale-95">
+            <div className="aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded bg-gray-100">
+              <img
+                src={file.source}
+                alt=""
+                className="object-cover hover:opacity-75 hover:cursor-zoom-in"
+              />
+            </div>
+          </div>
+
+          <p className="mt-2 block truncate text-sm font-medium text-gray-900 transition-all ease-in-out duration-700 group-hover:px-4">
+            {file.title}
+          </p>
+
+          <div className="w-full p-2 pt-0">
             <button
               tabIndex={-1}
               type="button"
-              className="absolute inset-0 focus:outline-none"
+              className="mt-4 w-full text-xs text-gray-500 font-light px-4 py-2 bg-gray-200 rounded transition-all ease-in-out duration-500 opacity-0 translate-y-2 hover:bg-gray-300 hover:text-gray-700 group-hover:opacity-100 group-hover:translate-y-0"
             >
-              <span className="sr-only">View details for {file.title}</span>
+              View Details
             </button>
           </div>
-          <p className="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">
-            {file.title}
-          </p>
         </li>
       ))}
     </ul>
--- src/components/Lists/VideoGrid.tsx
+++ src/components/Lists/VideoGrid.tsx
@@ -41,31 +41,37 @@ export default function VideoGrid() {
       {files.map((file) => (
         <li
           key={file.source}
-          className="relative p-2 rounded bg-gray-50 transition-all duration-300 shadow hover:shadow-lg"
+          className="group relative rounded-md hover:bg-gray-50 transition-all ease-in-out duration-500 hover:shadow-md"
         >
-          <div className="aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded bg-gray-100">
-            {file.isEmbedded ? (
-              <iframe className="object-cover" src={file.source} />
-            ) : (
-              <video
-                controls
-                autoPlay={false}
-                preload="off"
-                className="object-cover"
-                src={file.source}
-              />
-            )}
+          <div className="transition-transform ease-in-out duration-700 group-hover:scale-95">
+            <div className="aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded bg-gray-100">
+              {file.isEmbedded ? (
+                <iframe className="object-cover" src={file.source} />
+              ) : (
+                <video
+                  controls
+                  autoPlay={false}
+                  preload="off"
+                  className="object-cover"
+                  src={file.source}
+                />
+              )}
+            </div>
           </div>
-          <p className="mt-4 block truncate text-sm font-medium text-gray-900">
+
+          <p className="mt-2 block truncate text-sm font-medium text-gray-900 transition-all ease-in-out duration-700 group-hover:px-4">
             {file.title}
           </p>
-          <button
-            tabIndex={-1}
-            type="button"
-            className="mt-4 w-full text-xs text-gray-500 font-light px-4 py-2 bg-gray-200 rounded transition-all duration-300 hover:bg-gray-300 hover:text-gray-700 "
-          >
-            View Details
-          </button>
+
+          <div className="w-full p-2 pt-0">
+            <button
+              tabIndex={-1}
+              type="button"
+              className="mt-4 w-full text-xs text-gray-500 font-light px-4 py-2 bg-gray-200 rounded transition-all ease-in-out duration-500 opacity-0 translate-y-2 hover:bg-gray-300 hover:text-gray-700 group-hover:opacity-100 group-hover:translate-y-0"
+            >
+              View Details
+            </button>
+          </div>
         </li>
       ))}
     </ul>
