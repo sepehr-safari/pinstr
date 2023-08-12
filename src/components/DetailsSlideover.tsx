5b9f75645e844e7e9f2b48744bb673601ebabb60
Sepehr Safari <safari.sepehr@gmail.com>
2023-08-12 10:18:50 +0330

    edit details slideover

--- src/components/DetailsSlideover.tsx
+++ src/components/DetailsSlideover.tsx
@@ -1,13 +1,7 @@
-import { Dialog, Menu, Transition } from '@headlessui/react';
-import {
-  ChevronLeftIcon,
-  ChevronRightIcon,
-  EllipsisVerticalIcon,
-} from '@heroicons/react/20/solid';
+import { Dialog, Transition } from '@headlessui/react';
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid';
 import { XMarkIcon } from '@heroicons/react/24/outline';
-import { Fragment } from 'react';
-
-import { joinClassNames } from '@/utils';
+import { Fragment, useCallback, useEffect } from 'react';
 
 export default function DetailsSlideover({
   isShown,
@@ -15,13 +9,34 @@ export default function DetailsSlideover({
   onNext,
   onPrevious,
   details,
+  children,
 }: {
   isShown: boolean;
   onClose: () => void;
   onNext: () => void;
   onPrevious: () => void;
   details: any;
+  children: React.ReactNode;
 }) {
+  const handleKeyUp = useCallback(
+    (e: KeyboardEvent) => {
+      if (e.key === 'ArrowLeft') {
+        onPrevious();
+      } else if (e.key === 'ArrowRight') {
+        onNext();
+      }
+    },
+    [onPrevious, onNext]
+  );
+
+  useEffect(() => {
+    isShown && document.addEventListener('keyup', handleKeyUp);
+
+    return () => {
+      document.removeEventListener('keyup', handleKeyUp);
+    };
+  }, [handleKeyUp, isShown]);
+
   return (
     <Transition.Root show={isShown} as={Fragment}>
       <Dialog as="div" className="relative z-10" onClose={onClose}>
@@ -53,15 +68,16 @@ export default function DetailsSlideover({
               >
                 <Dialog.Panel className="pointer-events-auto w-screen max-w-2xl">
                   <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
-                    <div className="px-4 py-6 sm:px-6">
+                    <div className="px-4 py-6 sm:px-6 border-b">
                       <div className="flex items-start justify-between">
                         <Dialog.Title className="text-base font-semibold leading-6 text-gray-900">
                           {details.title || details.name || 'Details'}
                         </Dialog.Title>
                         <div className="ml-3 flex h-7 items-center">
                           <button
+                            tabIndex={-1}
                             type="button"
-                            className="relative rounded-md bg-white text-gray-400 hover:text-gray-500 focus:ring-2 focus:ring-indigo-500"
+                            className="relative rounded-md bg-white text-gray-400 hover:text-gray-500"
                             onClick={onClose}
                           >
                             <span className="absolute -inset-2.5" />
@@ -73,112 +89,7 @@ export default function DetailsSlideover({
                     </div>
                     {/* Main */}
                     <div className="divide-y divide-gray-200">
-                      <div className="pb-6">
-                        <div className="h-24 bg-indigo-700 sm:h-20 lg:h-28" />
-                        <div className="-mt-12 flow-root px-4 sm:-mt-8 sm:flex sm:items-end sm:px-6 lg:-mt-16">
-                          <div>
-                            <div className="-m-1 flex">
-                              <div className="inline-flex overflow-hidden rounded-lg border-4 border-white">
-                                <img
-                                  className="h-24 w-24 flex-shrink-0 sm:h-40 sm:w-40 lg:h-48 lg:w-48"
-                                  src="https://images.unsplash.com/photo-1501031170107-cfd33f0cbdcc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=256&h=256&q=80"
-                                  alt=""
-                                />
-                              </div>
-                            </div>
-                          </div>
-                          <div className="mt-6 sm:ml-6 sm:flex-1">
-                            <div>
-                              <div className="flex items-center">
-                                <h3 className="text-xl font-bold text-gray-900 sm:text-2xl">
-                                  Ashley Porter
-                                </h3>
-                                <span className="ml-2.5 inline-block h-2 w-2 flex-shrink-0 rounded-full bg-green-400">
-                                  <span className="sr-only">Online</span>
-                                </span>
-                              </div>
-                              <p className="text-sm text-gray-500">
-                                @ashleyporter
-                              </p>
-                            </div>
-                            <div className="mt-5 flex flex-wrap space-y-3 sm:space-x-3 sm:space-y-0">
-                              <button
-                                type="button"
-                                className="inline-flex w-full flex-shrink-0 items-center justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:flex-1"
-                              >
-                                Message
-                              </button>
-                              <button
-                                type="button"
-                                className="inline-flex w-full flex-1 items-center justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                              >
-                                Call
-                              </button>
-                              <div className="ml-3 inline-flex sm:ml-0">
-                                <Menu
-                                  as="div"
-                                  className="relative inline-block text-left"
-                                >
-                                  <Menu.Button className="relative inline-flex items-center rounded-md bg-white p-2 text-gray-400 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                                    <span className="absolute -inset-1" />
-                                    <span className="sr-only">
-                                      Open options menu
-                                    </span>
-                                    <EllipsisVerticalIcon
-                                      className="h-5 w-5"
-                                      aria-hidden="true"
-                                    />
-                                  </Menu.Button>
-                                  <Transition
-                                    as={Fragment}
-                                    enter="transition ease-out duration-100"
-                                    enterFrom="transform opacity-0 scale-95"
-                                    enterTo="transform opacity-100 scale-100"
-                                    leave="transition ease-in duration-75"
-                                    leaveFrom="transform opacity-100 scale-100"
-                                    leaveTo="transform opacity-0 scale-95"
-                                  >
-                                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                      <div className="py-1">
-                                        <Menu.Item>
-                                          {({ active }) => (
-                                            <a
-                                              href="#"
-                                              className={joinClassNames(
-                                                active
-                                                  ? 'bg-gray-100 text-gray-900'
-                                                  : 'text-gray-700',
-                                                'block px-4 py-2 text-sm'
-                                              )}
-                                            >
-                                              View profile
-                                            </a>
-                                          )}
-                                        </Menu.Item>
-                                        <Menu.Item>
-                                          {({ active }) => (
-                                            <a
-                                              href="#"
-                                              className={joinClassNames(
-                                                active
-                                                  ? 'bg-gray-100 text-gray-900'
-                                                  : 'text-gray-700',
-                                                'block px-4 py-2 text-sm'
-                                              )}
-                                            >
-                                              Copy profile link
-                                            </a>
-                                          )}
-                                        </Menu.Item>
-                                      </div>
-                                    </Menu.Items>
-                                  </Transition>
-                                </Menu>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
+                      <div className="py-6 bg-white">{children}</div>
                       <div className="px-4 py-5 sm:px-0 sm:py-0">
                         <dl className="space-y-8 sm:space-y-0 sm:divide-y sm:divide-gray-200">
                           <div className="sm:flex sm:px-6 sm:py-5">
@@ -226,7 +137,7 @@ export default function DetailsSlideover({
                           <button
                             type="button"
                             onClick={onPrevious}
-                            className="relative flex justify-center items-center rounded-l-md bg-white w-full py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-100 hover:text-gray-500"
+                            className="relative flex justify-center items-center bg-white w-full py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-100 hover:text-gray-500"
                           >
                             <ChevronLeftIcon
                               className="h-5 w-5"
@@ -237,7 +148,7 @@ export default function DetailsSlideover({
                           <button
                             type="button"
                             onClick={onNext}
-                            className="relative -ml-px flex justify-center items-center rounded-r-md bg-white w-full py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-100 hover:text-gray-500"
+                            className="relative -ml-px flex justify-center items-center bg-white w-full py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-100 hover:text-gray-500"
                           >
                             <span className="text-sm">Next</span>
                             <ChevronRightIcon
