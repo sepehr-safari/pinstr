14d3ed48a7e6ab7a3f4a3742434da72d8da0b452
Sepehr Safari <safari.sepehr@gmail.com>
2023-08-12 17:14:26 +0330

    edit utils index

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,4 @@
+export { default as formatRelativeTime } from './formatRelativeTime';
 export { default as joinClassNames } from './joinClassNames';
+export { default as parseAuthorsFromEvents } from './parseAuthorsFromEvents';
+export { default as parseBoardsFromEvents } from './parseBoardsFromEvents';
