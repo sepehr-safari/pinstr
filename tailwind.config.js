f93735b49bfc40c45be2edd470f849a688a78174
Sepehr Safari <safari.sepehr@gmail.com>
2023-08-13 22:53:12 +0330

    edit break points

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,13 @@ export default {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      screens: {
+        sm: '600px',
+        md: '900px',
+        lg: '1100px',
+        xl: '1300px',
+        '2xl': '1500px',
+      },
     },
   },
   plugins: [
