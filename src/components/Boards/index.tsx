43e415d169bfce80fcfc02fe506686a8f3d42230
Sepehr Safari <safari.sepehr@gmail.com>
2023-08-13 23:00:36 +0330

    edit boards fix gap

--- src/components/Boards/index.tsx
+++ src/components/Boards/index.tsx
@@ -71,7 +71,7 @@ export default function Boards({ fullWidth = false }: { fullWidth?: boolean }) {
       <div className="mx-auto pb-16 overflow-hidden max-w-md sm:max-w-none">
         <div
           className={joinClassNames(
-            'grid grid-cols-1 sm:grid-cols-2 sm:gap-y-8 sm:gap-x-6 md:grid-cols-3',
+            'grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3',
             fullWidth
               ? 'xl:grid-cols-4 2xl:grid-cols-5'
               : 'xl:grid-cols-2 2xl:grid-cols-3'
