14dbc129b84666eb3e2dac7858dbc40661999aac
Sepehr Safari <safari.sepehr@gmail.com>
2023-08-14 11:30:15 +0330

    fix logout remove query bug

--- src/queries/useUser.tsx
+++ src/queries/useUser.tsx
@@ -42,7 +42,7 @@ export default function useUser() {
       }
     },
     doLogout: () => {
-      queryClient.setQueryData(['app', 'user'], null);
+      queryClient.removeQueries(['app', 'user']);
     },
     user,
   };
