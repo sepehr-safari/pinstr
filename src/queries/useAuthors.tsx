3ffbc4b851708f1e58e7141d67ae4dd742a43194
Sepehr Safari <safari.sepehr@gmail.com>
2023-08-19 12:07:59 +0330

    edt useAuthor query disable refetch on mount

--- src/queries/useAuthors.tsx
+++ src/queries/useAuthors.tsx
@@ -25,6 +25,7 @@ export default function useAuthors(options?: {
     },
     refetchOnWindowFocus: false,
     enabled: options?.enabled && !!pool && !!relays,
+    refetchOnMount: false,
   });
 
   return { isAuthorsLoading: isLoading, authorsError: error, authors: data };
