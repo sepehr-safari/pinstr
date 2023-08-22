6426d8000f6d3a2714354f03def2c676b4ad72e0
Sepehr Safari <safari.sepehr@gmail.com>
2023-08-22 14:18:00 +0330

    edit profile page

--- src/pages/Layouts/Profile.tsx
+++ src/pages/Layouts/Profile.tsx
@@ -3,23 +3,28 @@ import { nip19 } from 'nostr-tools';
 import { Outlet, useParams } from 'react-router-dom';
 
 import { Breadcrumb } from '@/components/Navbars';
-import { useAuthors } from '@/queries';
+import { useAuthor } from '@/queries';
 
 export const Profile = () => {
   const { npub } = useParams();
-  const hex = npub ? nip19.decode(npub).data.toString() : null;
-  const { authors } = useAuthors({ authors: [hex!], enabled: !!hex });
-  const hasAuthor = !!authors && !!authors.length;
+
+  const hex = npub ? nip19.decode(npub).data.toString() : undefined;
+
+  const { data: author } = useAuthor(hex);
 
   return (
     <>
-      <div className="relative bg-gray-100 min-h-full rounded-t-md">
-        <div className="-mt-16 bg-gray-200 rounded-t-md">
-          <img
-            className="h-52 w-full object-cover object-center text-gray-200 md:rounded-t-md xl:h-64"
-            src={hasAuthor ? authors[0].banner : ''}
-            alt={hasAuthor ? authors[0].displayName : ' banner'}
-          />
+      <div className="relative bg-gray-100 min-h-[100vh] rounded-t-md">
+        <div className="-mt-16 bg-gray-300 rounded-t-md">
+          {author && author.banner ? (
+            <img
+              className="h-52 w-full object-cover object-center text-white md:rounded-t-md xl:h-64"
+              src={author ? author.banner : ''}
+              alt={author ? author.displayName : ' banner'}
+            />
+          ) : (
+            <div className="h-36 md:rounded-t-md xl:h-44" />
+          )}
         </div>
 
         <div className="mx-auto max-w-screen-4xl">
@@ -27,18 +32,18 @@ export const Profile = () => {
             <div className="px-6 py-10 flex flex-col items-center">
               <img
                 className="-mt-20 h-28 w-28 bg-gray-200 text-gray-200 object-cover object-center shadow-lg rounded-full absolute z-[2] xl:static xl:mt-0"
-                src={hasAuthor ? authors[0].picture : ''}
-                alt={hasAuthor ? authors[0].displayName : ' avatar'}
+                src={author ? author.picture : ''}
+                alt={author ? author.displayName : ' avatar'}
               />
 
               <h2 className="mt-12 text-lg font-semibold xl:mt-4">
-                {hasAuthor ? authors[0].displayName : ''}
+                {author ? author.displayName : ''}
               </h2>
               <span className="text-xs font-light text-gray-500">
-                {hasAuthor ? authors[0].nip05 : ''}
+                {author ? author.nip05 : ''}
               </span>
               <span className="mt-4 text-xs font-light text-gray-500 text-center">
-                {hasAuthor ? authors[0].about : ''}
+                {author ? author.about : ''}
               </span>
 
               <button className="mt-4 inline-flex justify-center items-center rounded-full bg-gray-900 px-6 py-2.5 text-xs font-semibold text-white shadow-sm hover:bg-gray-700">
