78046c25d33aa153867360ff82fd4318b629874a
Sepehr Safari <safari.sepehr@gmail.com>
2023-08-28 15:02:39 +0330

    fix textarea default value error

--- src/components/EditPinComponents/EditTextPin.tsx
+++ src/components/EditPinComponents/EditTextPin.tsx
@@ -44,7 +44,6 @@ export const EditTextPin = ({
             name="Content"
             id="Content"
             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
-            defaultValue={''}
             value={pin.value['Content']}
             onChange={(e) => {
               pin.set({ ...pin.value, Content: e.target.value });
