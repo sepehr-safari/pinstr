885540bf90326cd34abf3306b82ed086bed261e7
Sepehr Safari <safari.sepehr@gmail.com>
2023-08-29 22:56:58 +0330

    add a global board to zustand store for mutating

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,17 +1,45 @@
+import cloneDeep from 'lodash.clonedeep';
 import { SimplePool } from 'nostr-tools';
 import { create } from 'zustand';
+
+import { Board } from '@/types';
 
 interface State {
   pool: SimplePool;
   relays: string[];
+  board: Partial<Board>;
 }
 
 interface Actions {
   setRelays: (relays: string[]) => void;
+  setBoardItem: (key: keyof Board, value: any) => void;
+  setBoard: (board: Partial<Board>) => void;
+  setPin: (pinIndex: number, headerIndex: number, value: any) => void;
 }
 
 export const useLocalStore = create<State & Actions>((set) => ({
   pool: new SimplePool(),
   relays: ['wss://nos.lol'],
   setRelays: (relays) => set({ relays }),
+  board: {},
+  setBoardItem: (key, value) =>
+    set((state) => ({ board: { ...state.board, [key]: value } })),
+  setBoard: (board) => set({ board: cloneDeep(board) }),
+  setPin: (pinIndex, headerIndex, value) =>
+    set((state) => {
+      const newPins = [...(state.board.pins || [])];
+      if (newPins.length > pinIndex) {
+        newPins[pinIndex][headerIndex] = value;
+      } else {
+        newPins[pinIndex] = [];
+        newPins[pinIndex][headerIndex] = value;
+      }
+
+      return {
+        board: {
+          ...state.board,
+          pins: newPins,
+        },
+      };
+    }),
 }));
