b9f9792945eecc9f22871c0627cc335e82a85239
Sepehr Safari <safari.sepehr@gmail.com>
2023-08-30 16:16:43 +0330

    edit board summary card

--- src/components/BoardSummary.tsx
+++ src/components/BoardSummary.tsx
@@ -6,7 +6,7 @@ import { Link, useParams } from 'react-router-dom';
 import { useMutateBoardLike } from '@/mutations';
 import { useUser } from '@/queries';
 import { Board, Reactions } from '@/types';
-import { formatRelativeTime } from '@/utils';
+import { formatRelativeTime, loader } from '@/utils';
 
 export const BoardSummary = () => {
   const { npub, title } = useParams();
@@ -32,9 +32,9 @@ export const BoardSummary = () => {
   }
 
   return (
-    <div className="bg-white rounded-xl shadow-md mt-8 p-6 text-xs hidden xl:block">
+    <div className="overflow-hidden bg-white rounded-xl shadow-md text-xs hidden xl:block">
       <div className="w-full flex flex-col justify-between items-center">
-        <div className="w-full">
+        <div className="w-full py-4 px-4">
           <div className="inline-flex w-full justify-center items-center gap-1 text-xs font-light text-gray-400">
             <span>{board && formatRelativeTime(board.timestamp)}</span>
             <span>|</span>
@@ -46,7 +46,7 @@ export const BoardSummary = () => {
           </div>
 
           {board && board.tags.length > 0 && (
-            <div className="mt-2 flex justify-center gap-4 flex-wrap">
+            <div className="mt-1 flex justify-center gap-4 flex-wrap">
               {board.tags.map((tag, index) => (
                 <Link
                   to={`/t/${tag}`}
@@ -59,26 +59,37 @@ export const BoardSummary = () => {
             </div>
           )}
 
-          <div className="mt-6 border-t border-b py-6 flex gap-4 flex-col w-full items-center text-center">
-            <h3 className="text-base font-semibold tracking-tight text-gray-900">{title}</h3>
+          <div className="mt-2 border-t pt-4 flex flex-col w-full items-center text-center">
+            <div className="w-40 h-full rounded-md bg-gray-200 text-gray-200">
+              <img
+                src={board ? loader(board?.image, { w: 500, h: 400 }) : ''}
+                alt={title}
+                className="w-full h-full rounded-md"
+                loading="lazy"
+              />
+            </div>
 
-            <div className="flex text-xs font-light text-gray-600 text-center">
+            <h3 className="mt-2 text-base font-semibold tracking-tight leading-5 text-gray-900">
+              {title}
+            </h3>
+
+            <div className="mt-2 flex text-xs font-light text-gray-600 text-center">
               {board?.description}
             </div>
 
             {selfBoard && (
               <>
-                <div className="flex gap-2 shrink-0">
+                <div className="mt-6 mb-2 flex gap-2 shrink-0 w-full">
                   <button
                     type="button"
-                    className="rounded-md bg-gray-100 px-3 py-2 text-xs font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 hover:bg-gray-200 hover:text-gray-900"
+                    className="rounded-md bg-gray-100 w-full py-2 text-xs font-semibold text-gray-600 hover:bg-gray-200 hover:text-gray-900"
                     onClick={() => {}}
                   >
                     Edit Board
                   </button>
                   <button
                     type="button"
-                    className="rounded-md bg-gray-100 px-3 py-2 text-xs font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 hover:bg-gray-200 hover:text-gray-900"
+                    className="rounded-md bg-gray-100 w-full py-2 text-xs font-semibold text-gray-600 hover:bg-gray-200 hover:text-gray-900"
                     onClick={() => {}}
                   >
                     Add Pin
@@ -89,19 +100,19 @@ export const BoardSummary = () => {
           </div>
         </div>
 
-        <div className="mt-6 flex gap-4">
+        <div className="grid grid-cols-3 w-full h-10 divide-x border-t text-gray-600">
           <button
-            className="inline-flex justify-center items-center rounded-md bg-gray-100 ring-1 ring-gray-300 px-4 py-2 text-xs font-semibold text-gray-500 hover:shadow-md hover:text-gray-800"
+            className="inline-flex justify-center items-center text-xs font-semibold duration-200 hover:text-gray-900 hover:bg-gray-100"
             onClick={() => like()}
           >
             <HandThumbUpIcon className="mr-2 h-4 w-4" />
             <span className="">{reactions ? reactions.likes.length : 0}</span>
           </button>
-          <button className="inline-flex justify-center items-center rounded-md bg-gray-100 ring-1 ring-gray-300 px-4 py-2 text-xs font-semibold text-gray-500 hover:shadow-md hover:text-gray-800">
+          <button className="inline-flex justify-center items-center text-xs font-semibold duration-200 hover:text-gray-900 hover:bg-gray-100">
             <BoltIcon className="mr-2 h-4 w-4" />
             <span className="">{reactions ? reactions.zaps.length : 0}</span>
           </button>
-          <button className="inline-flex justify-center items-center rounded-md bg-gray-100 ring-1 ring-gray-300 px-4 py-2 text-xs font-semibold text-gray-500 hover:shadow-md hover:text-gray-800">
+          <button className="inline-flex justify-center items-center text-xs font-semibold duration-200 hover:text-gray-900 hover:bg-gray-100">
             <ChatBubbleLeftIcon className="mr-2 h-4 w-4" />
             <span className="">{reactions ? reactions.comments.length : 0}</span>
           </button>
