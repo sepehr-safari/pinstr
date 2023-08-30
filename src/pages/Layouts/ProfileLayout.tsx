6d77074e161d1a3a757fbc133b2b043659d667e9
Sepehr Safari <safari.sepehr@gmail.com>
2023-08-30 16:18:43 +0330

    edit profile layout

--- src/pages/Layouts/ProfileLayout.tsx
+++ src/pages/Layouts/ProfileLayout.tsx
@@ -2,6 +2,7 @@ import { PlusIcon } from '@heroicons/react/20/solid';
 import { nip19 } from 'nostr-tools';
 import { Outlet, useLocation, useParams } from 'react-router-dom';
 
+import { BoardSummary } from '@/components';
 import { Breadcrumb } from '@/components/Navbars';
 import { useAuthor } from '@/queries';
 import { joinClassNames, loader } from '@/utils';
@@ -12,54 +13,71 @@ const Banner = ({
 }: {
   banner: string | undefined;
   displayName: string | undefined;
-}) => (
-  <div className="-mt-20 absolute top-0 w-full bg-gray-300 rounded-t-md">
-    {!!banner ? (
-      <img
-        className="h-52 w-full object-cover text-white md:rounded-t-md xl:h-64"
-        src={loader(banner, { w: 1000, h: 256 })}
-        alt={`${displayName} banner`}
-        loading="lazy"
-      />
-    ) : (
-      <div className="h-52 md:rounded-t-md xl:h-64 bg-gradient-to-br from-purple-900 to-purple-600" />
-    )}
-  </div>
-);
+}) => {
+  const location = useLocation();
+  const state = location.state as { backgroundLocation?: Location };
+
+  return (
+    <div className="-mt-20 absolute top-0 w-full bg-gray-300 rounded-t-md">
+      {!!banner ? (
+        <img
+          className={joinClassNames(
+            'w-full object-cover text-white md:rounded-t-md',
+            state?.backgroundLocation ? 'h-40' : 'h-52'
+          )}
+          src={loader(banner, { w: 1000, h: 256 })}
+          alt={`${displayName} banner`}
+          loading="lazy"
+        />
+      ) : (
+        <div className="h-52 md:rounded-t-md xl:h-64 bg-gradient-to-br from-purple-900 to-purple-600" />
+      )}
+    </div>
+  );
+};
 
 const MaxWidthContainer = ({ children }: { children: React.ReactNode }) => (
-  <div className="mx-auto relative max-w-screen-4xl gap-14 p-0 flex flex-col xl:flex-row xl:p-14">
+  <div className="mx-auto relative max-w-screen-4xl gap-8 flex flex-col xl:flex-row xl:p-10">
     {children}
   </div>
 );
 
-const MainContainer = () => (
-  <div className="mt-0 w-full h-full px-8 pb-20 xl:mt-40 xl:px-0">
-    <div className="pb-8">
-      <Breadcrumb />
+const MainContainer = () => {
+  const location = useLocation();
+  const state = location.state as { backgroundLocation?: Location };
+
+  return (
+    <div
+      className={joinClassNames(
+        'w-full h-full px-8 pb-20 xl:px-0',
+        state?.backgroundLocation ? 'xl:mt-16' : 'xl:mt-28'
+      )}
+    >
+      <div className="mb-6">
+        <Breadcrumb />
+      </div>
+
+      <Outlet />
     </div>
+  );
+};
 
-    <Outlet />
-  </div>
-);
+const StickyContainer = ({ children }: { children: React.ReactNode }) => {
+  const location = useLocation();
+  const state = location.state as { backgroundLocation?: Location };
 
-const StickyContainer = ({
-  children,
-  backgroundLocation,
-}: {
-  children: React.ReactNode;
-  backgroundLocation?: Location | undefined;
-}) => (
-  <div
-    className={joinClassNames(
-      'mt-28 w-full xl:max-w-xs xl:mt-0',
-      'xl:self-start xl:sticky',
-      backgroundLocation ? 'xl:top-12' : 'xl:top-28'
-    )}
-  >
-    {children}
-  </div>
-);
+  return (
+    <div
+      className={joinClassNames(
+        'flex flex-col gap-4 w-full xl:max-w-xs',
+        'xl:self-start xl:sticky',
+        state?.backgroundLocation ? 'xl:top-10' : 'mt-24 xl:mt-12 xl:top-24'
+      )}
+    >
+      {children}
+    </div>
+  );
+};
 
 const ProfileCard = ({
   displayName,
@@ -74,39 +92,48 @@ const ProfileCard = ({
   about: string | undefined;
   npub: string | undefined;
 }) => (
-  <div className="px-6 py-10 flex flex-col items-center bg-white shadow-md z-[1] rounded-none xl:rounded-xl">
-    <img
-      className="-mt-16 h-24 w-24 bg-gray-200 text-gray-200 shadow-lg rounded-full absolute z-[2] xl:static xl:mt-0"
-      src={!!picture ? loader(picture, { w: 96, h: 96 }) : ''}
-      alt={`${displayName} avatar`}
-      loading="lazy"
-    />
+  <div className="p-6 flex flex-col items-center bg-white shadow-md z-[1] rounded-none xl:rounded-xl xl:items-start">
+    <div className="flex flex-col gap-4 w-full items-center xl:flex-row xl:items-stretch">
+      <a
+        href={`https://primal.net/${npub}`}
+        target="_blank"
+        rel="noreferrer"
+        className="-mt-16 overflow-hidden shrink-0 h-24 w-24 bg-gray-200 text-gray-200 shadow-lg rounded-full absolute z-[2] xl:static xl:mt-0 hover:cursor-pointer"
+      >
+        <div className="relative">
+          <div className="absolute duration-300 text-white text-xs text-center font-semibold inset-0 flex justify-center items-center opacity-0 hover:opacity-100 bg-black/70">
+            Open With Primal
+          </div>
+          <img
+            src={!!picture ? loader(picture, { w: 96, h: 96 }) : ''}
+            alt={`${displayName} avatar`}
+            loading="lazy"
+          />
+        </div>
+      </a>
 
-    <h2 className="mt-12 text-lg font-semibold xl:mt-4">{displayName || ''}</h2>
-    <span className="text-xs font-light text-gray-500">{nip05 || ''}</span>
-    <span className="mt-4 text-xs font-light text-gray-700 text-center">{about || ''}</span>
+      <div className="mt-12 w-full text-center xl:text-start xl:mt-0 xl:flex xl:flex-col xl:justify-around">
+        <h2 className="text-lg font-semibold xl:leading-none">{displayName || ''}</h2>
+        <span className="mt-1 text-xs font-light text-gray-500">{nip05 || ''}</span>
 
-    <button className="mt-4 inline-flex justify-center items-center rounded-full bg-gray-900 px-6 py-2.5 text-xs font-semibold text-white shadow-sm hover:bg-gray-700">
+        <button className="mt-1 hidden w-full xl:inline-flex justify-center items-center rounded-full bg-gray-900 py-2 text-xs font-semibold text-white shadow-sm hover:bg-gray-700">
+          <PlusIcon className="-ml-1 w-4 h-4" />
+          <span className="ml-1">Follow</span>
+        </button>
+      </div>
+    </div>
+
+    <span className="mt-4 text-xs font-light text-gray-700 xl:mt-6">{about || ''}</span>
+
+    <button className="mt-4 inline-flex xl:hidden justify-center items-center rounded-full bg-gray-900 px-6 py-2 text-xs font-semibold text-white shadow-sm hover:bg-gray-700">
       <PlusIcon className="-ml-1 w-4 h-4" />
       <span className="ml-1">Follow</span>
     </button>
-
-    <a
-      href={`https://primal.net/${npub}`}
-      target="_blank"
-      rel="noreferrer"
-      className="mt-4 text-xs font-medium text-gray-400 hover:text-gray-600 hover:underline"
-    >
-      Open with Primal
-    </a>
   </div>
 );
 
 export const ProfileLayout = () => {
   const { npub } = useParams();
-
-  const location = useLocation();
-  const state = location.state as { backgroundLocation?: Location };
 
   const hex = npub ? nip19.decode(npub).data.toString() : undefined;
 
@@ -119,7 +146,7 @@ export const ProfileLayout = () => {
         <Banner banner={banner} displayName={displayName} />
 
         <MaxWidthContainer>
-          <StickyContainer backgroundLocation={state?.backgroundLocation}>
+          <StickyContainer>
             <ProfileCard
               about={about}
               displayName={displayName}
@@ -128,7 +155,7 @@ export const ProfileLayout = () => {
               picture={picture}
             />
 
-            {/* <BoardSummary /> */}
+            <BoardSummary />
           </StickyContainer>
 
           <MainContainer />
