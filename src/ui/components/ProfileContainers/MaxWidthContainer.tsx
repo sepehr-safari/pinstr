5412fcf9cf480d122379acc841ea50d0951508cc
Sepehr Safari <safari.sepehr@gmail.com>
2023-08-31 23:46:33 +0330

    add a little space around cards in profile

--- src/ui/components/ProfileContainers/MaxWidthContainer.tsx
+++ src/ui/components/ProfileContainers/MaxWidthContainer.tsx
@@ -1,5 +1,5 @@
 export const MaxWidthContainer = ({ children }: { children: React.ReactNode }) => (
-  <div className="mx-auto relative max-w-screen-4xl gap-6 flex flex-col xl:flex-row xl:p-6">
+  <div className="mx-auto relative max-w-screen-4xl gap-y-6 gap-x-8 flex flex-col xl:flex-row xl:p-8">
     {children}
   </div>
 );
