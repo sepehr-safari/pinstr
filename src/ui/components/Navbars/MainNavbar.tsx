1338ade5f176cae64a601ae08c8c896c521b426b
Sepehr Safari <safari.sepehr@gmail.com>
2023-09-03 23:50:32 +0330

    turn off autoComplete for searchbar

--- src/ui/components/Navbars/MainNavbar.tsx
+++ src/ui/components/Navbars/MainNavbar.tsx
@@ -50,11 +50,12 @@ export const MainNavbar = () => {
                     <MagnifyingGlassIcon className="h-5 w-5 text-gray-500" aria-hidden="true" />
                   </div>
                   <input
+                    type="search"
                     id="search"
                     name="search"
                     className="block w-full rounded-md border-0 bg-opacity-30 bg-white py-3 pl-10 pr-3 text-xs text-gray-900 ring-1 ring-inset ring-gray-900/20 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-gray-500 focus:bg-opacity-50"
                     placeholder="Search boards, tags, and people"
-                    type="search"
+                    autoComplete="off"
                   />
                 </div>
               </div>
