1bc0f3eed93a2164fc9f1d28c8fed8c9a811e999
Sepehr Safari <safari.sepehr@gmail.com>
2023-09-10 09:38:58 +0330

    add vite config for manual chunks

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,15 @@ export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-stuff': ['react', 'react-dom', 'react-router-dom'],
+          'nostr-tools': ['nostr-tools'],
+          'react-player': ['react-player'],
+        },
+      },
+    },
+  },
 });
