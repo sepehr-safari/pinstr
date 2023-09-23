02a986cd7e5e1eb92518e19e99876cf3ac6300b3
Sepehr Safari <safari.sepehr@gmail.com>
2023-09-23 18:14:10 +0330

    fix missing export

--- src/logic/mutations/index.tsx
+++ src/logic/mutations/index.tsx
@@ -3,5 +3,6 @@ export * from './useMutateBoardComment';
 export * from './useMutateBoardLike';
 export * from './useMutateNoteComment';
 export * from './useMutateNoteLike';
+export * from './useMutateSettings';
 export * from './useMutateUser';
 export * from './usePublish';
