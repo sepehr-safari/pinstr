552953bb9c14e8d0362902f034dcc52a07b7e432
Sepehr Safari <safari.sepehr@gmail.com>
2023-10-22 16:40:12 +0330

    refactor store and remove global board state

--- src/logic/store/index.tsx
+++ src/logic/store/index.tsx
@@ -1,9 +1,7 @@
 import NDK, { NDKNip07Signer } from '@nostr-dev-kit/ndk';
 import NDKCacheAdapterDexie from '@nostr-dev-kit/ndk-cache-dexie';
-import cloneDeep from 'lodash.clonedeep';
 import { create } from 'zustand';
 
-import { NDKBoard } from '@/logic/types';
 import { SimplePool } from 'nostr-tools';
 
 const dexieAdapter = new NDKCacheAdapterDexie({ dbName: 'pinstr-db' });
@@ -11,7 +9,6 @@ const dexieAdapter = new NDKCacheAdapterDexie({ dbName: 'pinstr-db' });
 type AppState = {
   pool: SimplePool;
   relays: string[];
-  board: Partial<NDKBoard>;
 };
 
 type NDKState = {
@@ -20,15 +17,11 @@ type NDKState = {
 
 type Actions = {
   setRelays: (relays: string[]) => void;
-  setBoardItem: (key: keyof NDKBoard, value: any) => void;
-  setBoard: (board: Partial<NDKBoard>) => void;
-  setPin: (pinIndex: number, headerIndex: number, value: any) => void;
 };
 
 export const useLocalStore = create<AppState & NDKState & Actions>((set) => ({
   pool: new SimplePool(),
   relays: ['wss://nos.lol'],
-  board: {},
   ndk: new NDK({
     signer: new NDKNip07Signer(),
     cacheAdapter: dexieAdapter,
@@ -37,23 +30,4 @@ export const useLocalStore = create<AppState & NDKState & Actions>((set) => ({
     autoFetchUserMutelist: false,
   }),
   setRelays: (relays) => set({ relays }),
-  setBoardItem: (key, value) => set((state) => ({ board: { ...state.board, [key]: value } })),
-  setBoard: (board) => set({ board: cloneDeep(board) }),
-  setPin: (pinIndex, headerIndex, value) =>
-    set((state) => {
-      const newPins = [...(state.board.pins || [])];
-      if (newPins.length > pinIndex) {
-        newPins[pinIndex][headerIndex] = value;
-      } else {
-        newPins[pinIndex] = [];
-        newPins[pinIndex][headerIndex] = value;
-      }
-
-      return {
-        board: {
-          ...state.board,
-          pins: newPins,
-        },
-      };
-    }),
 }));
