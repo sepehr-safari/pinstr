000d058eddb3dcbead4aa927f7a5538e20156f09
Sepehr Safari <safari.sepehr@gmail.com>
2023-11-08 14:34:05 +0330

    improve button component

--- src/shared/components/button/index.tsx
+++ src/shared/components/button/index.tsx
@@ -5,9 +5,11 @@ const variantClasses = {
   secondary: 'bg-gray-100 text-gray-600 hover:bg-gray-200 hover:text-gray-900',
   accent: 'bg-purple-800 text-white hover:bg-purple-600',
   outline: 'bg-transparent text-gray-900 border border-gray-300 hover:bg-gray-100',
+  text: 'bg-transparent text-gray-900 hover:underline',
 };
 
 const sizeClasses = {
+  none: 'p-0',
   sm: 'px-2 py-1.5',
   md: 'px-3 py-2',
   lg: 'px-5 py-2.5',
@@ -38,8 +40,8 @@ export const Button = ({
 }: Props) => {
   const joinedClassNames = joinClassNames(
     'flex items-center justify-center gap-2 text-xs font-semibold focus:outline-none',
-    variantClasses[variant],
     sizeClasses[size],
+    variantClasses[variant],
     rounded ? 'rounded-full' : 'rounded-md',
     disabled ? 'opacity-50 cursor-not-allowed' : '',
     block ? 'w-full' : '',
