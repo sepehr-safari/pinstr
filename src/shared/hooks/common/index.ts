963e3336711aa7a625096fff936ef0a0b2b567fa
Sepehr Safari <safari.sepehr@gmail.com>
2023-11-08 19:35:23 +0330

    remove hooks create and edit pin params

--- src/shared/hooks/common/index.ts
+++ src/shared/hooks/common/index.ts
@@ -1,7 +1,5 @@
 export * from './useClickAway';
 export * from './useCommentsParams';
-export * from './useCreatePinParams';
-export * from './useEditPinParams';
 export * from './useFiltersParams';
 export * from './useRemoveBoardParams';
 export * from './useRemovePinParams';
