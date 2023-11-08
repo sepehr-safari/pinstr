e25cc872a6d4fd9e555c6655d7f2dcdbc5c7acf0
Sepehr Safari <safari.sepehr@gmail.com>
2023-11-08 14:41:54 +0330

    add template for pin wizard

---
+++ src/features/pin-wizard/index.tsx
@@ -0,0 +1,19 @@
+import { Board } from '@/shared/types';
+
+type Props = {
+  board: Board;
+  pinIndex: number;
+};
+
+export const PinWizard = ({ board, pinIndex }: Props) => {
+  console.log('board', board);
+  console.log('pinIndex', pinIndex);
+
+  return (
+    <>
+      {
+        //
+      }
+    </>
+  );
+};
