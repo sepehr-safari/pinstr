852c8e493876c506ec89dbd49cb9c328063b0422
Sepehr Safari <safari.sepehr@gmail.com>
2023-11-11 15:44:25 +0330

    fix modal overflow

--- src/shared/components/modal/index.tsx
+++ src/shared/components/modal/index.tsx
@@ -23,7 +23,7 @@ export const Modal = ({ onClose, children }: Params) => {
         </Transition.Child>
 
         <div className="fixed inset-0 z-10 overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div className="flex min-h-full items-center justify-center p-4 text-center sm:p-0">
             <Transition.Child
               as={Fragment}
               enter="duration-300"
@@ -33,7 +33,7 @@ export const Modal = ({ onClose, children }: Params) => {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl duration-200 sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+              <Dialog.Panel className="relative transform rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl duration-200 sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
                 {children}
               </Dialog.Panel>
             </Transition.Child>
