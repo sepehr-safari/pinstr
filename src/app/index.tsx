0606435ab767bbbcfcde05c0abe09137a4c53838
Sepehr Safari <safari.sepehr@gmail.com>
2024-03-25 09:58:47 +0330

    update app providers

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,42 +1,23 @@
-import { QueryClient } from '@tanstack/react-query';
-import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
-import { useEffect } from 'react';
+import { useNostrHooks } from 'nostr-hooks';
 import { BrowserRouter } from 'react-router-dom';
-
-import { AppRouter } from '@/pages';
-import { useLocalStore } from '@/shared/store';
-import { Toast } from '@/shared/components';
-import { createIDBPersister } from '@/shared/utils';
 
 import './index.css';
 
-const persister = createIDBPersister();
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      // cacheTime: 1000 * 60 * 60 * 48, // 48 hours
-      // staleTime: 1000 * 60 * 1, // 1 minute
-      cacheTime: Infinity,
-      staleTime: Infinity,
-      // refetchOnWindowFocus: false,
-    },
-  },
-});
+import { AppRouter } from '@/pages';
+
+import { ThemeProvider } from '@/shared/components/theme-provider';
+import { Toaster } from '@/shared/components/ui/toaster';
 
 export const App = () => {
-  const ndk = useLocalStore((state) => state.ndk);
-
-  useEffect(() => {
-    if (ndk) ndk.connect();
-  }, [ndk]);
+  useNostrHooks();
 
   return (
-    <PersistQueryClientProvider client={queryClient} persistOptions={{ persister }}>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <BrowserRouter>
         <AppRouter />
       </BrowserRouter>
 
-      <Toast />
-    </PersistQueryClientProvider>
+      <Toaster />
+    </ThemeProvider>
   );
 };
