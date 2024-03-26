82137d3e7a73c2ac888df494047d45b6086e02f4
Sepehr Safari <safari.sepehr@gmail.com>
2024-03-26 13:32:02 +0330

    add custom ndk

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,6 @@
-import { NDKPrivateKeySigner } from '@nostr-dev-kit/ndk';
-import { useNdk, useNostrHooks } from 'nostr-hooks';
-import { useEffect } from 'react';
+import NDK, { NDKPrivateKeySigner, NDKSigner } from '@nostr-dev-kit/ndk';
+import NDKCacheAdapterDexie from '@nostr-dev-kit/ndk-cache-dexie';
+import { useNostrHooks } from 'nostr-hooks';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
@@ -10,14 +10,22 @@ import { AppRouter } from '@/pages';
 import { ThemeProvider } from '@/shared/components/theme-provider';
 import { Toaster } from '@/shared/components/ui/toaster';
 
-export const App = () => {
-  useNostrHooks();
-  const { setSigner } = useNdk();
+let signer: NDKSigner | undefined = undefined;
+const pk = localStorage.getItem('pk');
+if (pk) {
+  signer = new NDKPrivateKeySigner(pk);
+}
 
-  useEffect(() => {
-    const pk = localStorage.getItem('pk');
-    pk && setSigner(new NDKPrivateKeySigner(pk));
-  }, [setSigner]);
+const ndk = new NDK({
+  autoConnectUserRelays: false,
+  autoFetchUserMutelist: false,
+  cacheAdapter: new NDKCacheAdapterDexie({ dbName: 'pinstr' }),
+  explicitRelayUrls: ['wss://nos.lol'],
+  signer,
+});
+
+export const App = () => {
+  useNostrHooks(ndk);
 
   return (
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
